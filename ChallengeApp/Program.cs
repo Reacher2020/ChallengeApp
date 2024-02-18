88cd98dba33416a0eb842a8a81750ae1c3d0da6f
Paweł Sawicki <pksawicki@gmail.com>
2024-02-18 19:30:03 +0100

    dzien 4

--- ChallengeApp/Program.cs
+++ ChallengeApp/Program.cs
@@ -1,2 +1,17 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿string name = "Ewa";
+var plec = 'm'; //m - merzczyzna f - kobieta
+int myAge = 15;
+
+
+if ((plec == 'f') && (myAge < 30))
+{
+    Console.WriteLine("Kobieta ponizej 30 lat");
+};
+if ((name == "Ewa") && (myAge == 30))
+{ 
+    Console.WriteLine("Ewa lat 30");
+};
+if ((plec == 'm') && (myAge < 18))
+{
+    Console.WriteLine("Małoletni mężczyzna");
+};
