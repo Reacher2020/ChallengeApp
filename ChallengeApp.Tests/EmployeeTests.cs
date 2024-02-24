8e5c519ef0a95cbb439ed4a6d7d281208f0d661c
Paweł Sawicki <pksawicki@gmail.com>
2024-02-24 19:29:26 +0100

    dzien 9 Ostateczna versja

--- ChallengeApp.Tests/EmployeeTests.cs
+++ ChallengeApp.Tests/EmployeeTests.cs
@@ -1,12 +1,12 @@
 namespace ChallengeApp.Tests
 {
     public class EmployeeTestTest
-    { 
-    [Test]
-    public void WhenGetStatistics_ShouldReturnCorectMax()
+    {
+        [Test]
+        public void WhenGetStatistics_ShouldReturnCorectMax()
         {
             //Arrange
-            var employee = new Employee("Anna", "Powier¿a", 37);
+            var employee = new Employee("Adam", "Kowalski", 42);
             employee.AddGrade(10.98f);
             employee.AddGrade(4.02f);
 
@@ -18,36 +18,36 @@ namespace ChallengeApp.Tests
             Assert.AreEqual(10.98f, statistics.Max);
         }
 
-    [Test]
-    public void WhenGetStatistics_ShouldReturnCorectMin()
+        [Test]
+        public void WhenGetStatistics_ShouldReturnCorectMin()
         {
             //Arrange
             var employee = new Employee("Adam", "Kowalski", 42);
             employee.AddGrade(10.98f);
-            employee.AddGrade4.02f);
+            employee.AddGrade(4.02f);
 
             //Act
             var statistics = new Statistics();
             statistics = employee.GetStatistics();
 
             //Assert
-            Assert.AreEqual(-7.90f, statistics.Min);
+            Assert.AreEqual(4.02f, statistics.Min);
         }
-    
-    [Test]
-    public void WhenGetStatistics_ShouldReturnCorectAverange()
-    {
-        //Arrange
-        var employee = new Employee("Adam", "Kowalski", 42);
-        employee.AddGrade(10.98f);
-        employee.AddGrade(4.02f);
 
-        //Act
-        var statistics = new Statistics();
-        statistics = employee.GetStatistics();
+        [Test]
+        public void WhenGetStatistics_ShouldReturnCorectAverange()
+        {
+            //Arrange
+            var employee = new Employee("Adam", "Kowalski", 42);
+            employee.AddGrade(10.98f);
+            employee.AddGrade(4.02f);
 
-        //Assert
-        Assert.AreEqual(-7.5f, statistics.Average);
+            //Act
+            var statistics = new Statistics();
+            statistics = employee.GetStatistics();
+
+            //Assert
+            Assert.AreEqual(7.5f, statistics.Average);
+        }
     }
-
 }
